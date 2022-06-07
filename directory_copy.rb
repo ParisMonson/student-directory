78931898b800472f4dd57005465070e03ac62e4d
EC2 Default User <ec2-user@ip-172-31-71-40.ec2.internal>
2022-06-07 12:30:50 +0000

    Created a copy of directory.rb. In this copy I have implemented the changes for Step 8 questions.

---
+++ directory_copy.rb
@@ -0,0 +1,59 @@
+students = []
+
+def input_students
+  puts "Please enter the name of the student"
+  students = []
+  name = gets.chomp
+  puts "Please enter their cohort:"
+  cohort = gets.chomp
+  while !name.empty? do
+    
+    students << {name: name, cohort: cohort.to_sym}
+    puts "Now we have #{students.count} students"
+    puts "Please enter the name of the student"
+    name = gets.chomp
+    puts "Please enter their cohort:"
+    cohort = gets.chomp
+    cohort == "" ? cohort = "Unknown" : nil
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villain Academy".center(100)
+  puts "--------------------------".center(100)
+end
+
+def print(names)
+  names_printed = 0
+  cohort_list = []
+  
+  for i in names do
+    i.each { |k, v|
+      k == :cohort && !cohort_list.include?(v) ? cohort_list << v : nil
+    }
+    
+  end
+  
+  cohort_list.each do |cohort|
+    names.each do |hash|
+      hash.each do |k, v|
+        if k == :cohort && v == cohort
+          puts "#{names_printed + 1}. #{hash[:name]} (#{hash[:cohort]})".center(100)
+          names_printed += 1
+        end
+    end
+  end
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students".center(100)
+  puts "--------------------------".center(100)
+end
+
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
